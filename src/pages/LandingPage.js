e889b2403105e8276a4cac5c07286b46c90e47b0
Andrew Williams <andrew.k.w@outlook.com>
2023-03-13 14:34:39 +0000

    pages folder

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ArticleList from '../components/ArticleList';
+const LandingPage = () => {
+  return (
+    <div>
+      <ArticleList />
+    </div>
+  );
+};
+
+export default LandingPage;
