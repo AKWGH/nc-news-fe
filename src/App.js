33799cbf09e2c62bd9b7bc8a180fa503492d2086
Andrew Williams <andrew.k.w@outlook.com>
2023-03-14 08:50:02 +0000

    changed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import Header from './components/Header';
 import LandingPage from './pages/LandingPage';
+import { Routes, Route } from 'react-router-dom';
+import IndividualArticlePage from './pages/IndividualArticlePage';
+import ArticlesPage from './pages/ArticlesPage';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <LandingPage />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/articles" element={<ArticlesPage />} />
+        <Route
+          path="/articles/:article_id"
+          element={<IndividualArticlePage />}
+        />
+      </Routes>
     </div>
   );
 }
