503915845b8046fb643bab1ab019c7d83e1d59f7
Andrew Williams <andrew.k.w@outlook.com>
2023-03-14 10:15:52 +0000

    added commentList component to be rendered

--- src/pages/IndividualArticlePage.js
+++ src/pages/IndividualArticlePage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { getIndividualArticle } from '../utilsAPI';
 import { useEffect, useState } from 'react';
+import CommentsList from '../components/CommentsList';
 
 const IndividualArticlePage = () => {
   const { article_id } = useParams();
@@ -14,13 +15,18 @@ const IndividualArticlePage = () => {
   }, []);
 
   return (
-    <section>
-      <h1>{articleData.title}</h1>
-      <img src={articleData.article_img_url} alt="" />
-      <p>{articleData.body}</p>
-      <p>votes {articleData.votes}</p>
-      <p>comments {articleData.comment_count}</p>
-    </section>
+    <>
+      <section>
+        <h1>{articleData.title}</h1>
+        <img src={articleData.article_img_url} alt="" />
+        <p>{articleData.body}</p>
+        <p>votes {articleData.votes}</p>
+        <p>comments {articleData.comment_count}</p>
+      </section>
+      <section>
+        <CommentsList article_id={article_id} />
+      </section>
+    </>
   );
 };
 
