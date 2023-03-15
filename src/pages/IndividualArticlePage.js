12874e764b6ea0d02cd2d4ea77bf7a67180f1428
Andrew Williams <andrew.k.w@outlook.com>
2023-03-15 10:58:41 +0000

    allowed voting functionality to work in my app

--- src/pages/IndividualArticlePage.js
+++ src/pages/IndividualArticlePage.js
@@ -1,26 +1,43 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { getIndividualArticle } from '../utilsAPI';
+import { getIndividualArticle, voteForArticle } from '../utilsAPI';
 import { useEffect, useState } from 'react';
 import CommentsList from '../components/CommentsList';
 
 const IndividualArticlePage = () => {
   const { article_id } = useParams();
   const [articleData, setArticleData] = useState([]);
+  const [userVote, setUserVote] = useState(0);
+
+  const onClickHandler = () => {
+    setUserVote(1);
+    voteForArticle(article_id).catch((err) => {
+      setUserVote(0);
+    });
+  };
 
   useEffect(() => {
     getIndividualArticle(article_id).then((data) => {
       setArticleData(data);
     });
-  }, []);
+  }, [article_id]);
 
   return (
     <>
       <section>
         <h1>{articleData.title}</h1>
         <img src={articleData.article_img_url} alt="" />
+        <p>votes {articleData.votes + userVote} </p>
+        <button
+          disabled={userVote !== 0}
+          onClick={() => {
+            onClickHandler();
+          }}
+        >
+          vote
+        </button>
         <p>{articleData.body}</p>
-        <p>votes {articleData.votes}</p>
+
         <p>comments {articleData.comment_count}</p>
       </section>
       <section>
