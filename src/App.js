6e262e8d069c110244885c6eae3fecf41a6b090a
Andrew Williams <andrew.k.w@outlook.com>
2023-03-29 19:39:27 +0100

    switch active user

--- src/App.js
+++ src/App.js
@@ -4,17 +4,24 @@ import LandingPage from './pages/LandingPage';
 import { Routes, Route } from 'react-router-dom';
 import IndividualArticlePage from './pages/IndividualArticlePage';
 import ArticlesPage from './pages/ArticlesPage';
+import { useState } from 'react';
 
 function App() {
+  const [username, setUsername] = useState('jessjelly');
   return (
     <div className="App">
       <Header />
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route
+          path="/"
+          element={
+            <LandingPage setUsername={setUsername} username={username} />
+          }
+        />
         <Route path="/articles" element={<ArticlesPage />} />
         <Route
           path="/article/:article_id"
-          element={<IndividualArticlePage />}
+          element={<IndividualArticlePage username={username} />}
         />
         <Route path="/articles/:filtered" element={<ArticlesPage />} />
       </Routes>
